eb830fa92c1cc1ece094202cb5aa304ee320b0a3
Nikhil Ugale <nikhil10.ugale@gmail.com>
2021-09-20 22:25:32 +0530

    static image

--- basic.py
+++ basic.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template('basic.html')
+    name = 'sir'
+    letters = list(name)
+    return render_template('basic.html',name=name,letters=letters)
 
 if __name__ == '__main__':
     app.run(debug=True)
